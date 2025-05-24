b04d5239baf1dcfe524656caded2a7db2a4e05c5
Isak Ellmer <isak01@gmail.com>
2025-05-24 13:34:26 +0200

    Fix width of columns depending on size of doubles

--- programs/clop/src/real/CRealObserver.cpp
+++ programs/clop/src/real/CRealObserver.cpp
@@ -23,6 +23,34 @@
 /////////////////////////////////////////////////////////////////////////////
 void CRealObserver::OnOutcome(int i) {
 
+	std::vector<int> width;
+	std::vector<double> v(paramcol.GetSize());
+	me.ComputeLocalWeights();
+	bool fMax = me.MaxParameter(&v[0]);
+	double Rating, Variance;
+	reg.GetPosteriorInfo(&v[0], Rating, Variance);
+
+	for (int j = 0; j < paramcol.GetSize(); j++) {
+		if (fMax) {
+			const CParameter &param = paramcol.GetParam(j);
+			double upper = param.TransformFromQLR(1.0);
+			double lower = param.TransformFromQLR(-1.0);
+			int w = 1;
+			if (upper > 0.5)
+				w = std::lround(std::max(log10(upper), (double)w));
+			if (upper < -0.5)
+				w = std::lround(std::max(1 + log10(-upper), (double)w));
+			if (lower > 0.5)
+				w = std::lround(std::max(log10(lower), (double)w));
+			if (lower < -0.5)
+				w = std::lround(std::max(1 + log10(-lower), (double)w));
+			width.push_back(2 * w + 3 + 3 + 2 + 2);
+		}
+		else {
+			width.push_back(0);
+		}
+	}
+
 	headers = headers % 20;
 	if (headers == 0) {
 		std::cout << '\n';
@@ -30,7 +58,7 @@ void CRealObserver::OnOutcome(int i) {
 		std::cout << std::setw(13) << "Elo";
 		for (int j = 0; j < paramcol.GetSize(); j++) {
 			const CParameter &param = paramcol.GetParam(j);
-			std::cout << std::setw(std::max((int)param.GetName().length() + 2, 17)) << param.GetName();
+			std::cout << std::setw(std::max(std::max((int)param.GetName().length() + 2, 17), width[j])) << param.GetName();
 		}
 		std::cout << "\n\n";
 	}
@@ -38,12 +66,6 @@ void CRealObserver::OnOutcome(int i) {
 
 	results.Refresh();
 	std::cout << std::setw(8) << results.GetSamples();
-
-	std::vector<double> v(paramcol.GetSize());
-	me.ComputeLocalWeights();
-	bool fMax = me.MaxParameter(&v[0]);
-	double Rating, Variance;
-	reg.GetPosteriorInfo(&v[0], Rating, Variance);
 
 	std::cout << std::fixed << std::setprecision(1);
 	if (fMax) {
@@ -53,7 +75,7 @@ void CRealObserver::OnOutcome(int i) {
 		double PlusMinus = c * Deviation;
 		double Elo = Rating * 400.0 / std::log(10.0);
 		double EloPlusMinus = PlusMinus * 400.0 / std::log(10.0);
-		
+
 		std::ostringstream oss;
 		oss << std::fixed << std::setprecision(1);
 		oss << Elo << "+-" << EloPlusMinus;
@@ -86,7 +108,7 @@ void CRealObserver::OnOutcome(int i) {
 			oss << std::fixed << std::setprecision(2);
 			oss << value << "+-" << plusminus;
 
-			std::cout << std::setw(std::max((int)param.GetName().length() + 2, 17)) << oss.str();
+			std::cout << std::setw(std::max(std::max((int)param.GetName().length() + 2, 17), width[j])) << oss.str();
 		}
 	}
 
